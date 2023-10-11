41dd528355808be3eb3b71dd377b278b9f7b3d13
jimmyshoubaki <jimmyshoubaki@gmail.com>
2023-10-11 18:28:17 -0500

    updated sidebar text

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,7 +6,7 @@ const DashboardLayout = ({
     return ( <div className="h-full relative">
         <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
             <div>
-                Hello Sidebar
+                Hello Sidebar!
             </div>
         </div>
     </div>
